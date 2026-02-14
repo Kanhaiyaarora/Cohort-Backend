797445d815e3e84626916cb486babe95fbfdccab
Kanhaiya Arora <kanhaiyaarora75@gmail.com>
2026-02-14 11:50:30 +0530

    create post api done

--- Day-13/src/controllers/post.controller.js
+++ Day-13/src/controllers/post.controller.js
@@ -1,18 +1,45 @@
 const { toFile } = require("@imagekit/nodejs");
 const imageKit = require("@imagekit/nodejs");
+const postModel = require("../models/post.model");
+const jwt = require("jsonwebtoken");
 
 const imagekit = new imageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
 
 async function createPostController(req, res) {
-  console.log(req.body, req.file);
+  const token = req.cookies.token;
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Token not found. Unauthorised access" });
+  }
+
+  let decode = null;
+  try {
+    decode = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "token not valid - unauthorised access" });
+  }
 
   const file = await imagekit.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
-    filename: "Test",
+    fileName: "Test",
+    folder: "insta-clone-post",
   });
-  res.send(file);
+
+  const post = await postModel.create({
+    caption: req.body.caption,
+    imageUrl: file.url,
+    user: decode.id,
+  });
+
+  res.status(201).json({
+    message: "Post created successfully",
+    post,
+  });
 }
 
 module.exports = { createPostController };
