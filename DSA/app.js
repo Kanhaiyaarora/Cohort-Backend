ed5f13605e248e278cafa5d0d4de6fa54e7acfa3
Kanhaiya Arora <kanhaiyaarora75@gmail.com>
2026-06-15 18:56:25 +0530

    swap two no.

--- DSA/app.js
+++ DSA/app.js
@@ -1,2 +1,13 @@
-let otp = Math.trunc(Math.random() * 9000 + 1000); // 1000-9999 // trunc i sused for removing decimal part
-console.log(otp);
+// 4 digit OTP generator
+
+// let otp = Math.trunc(Math.random() * 9000 + 1000); // 1000-9999 // trunc is used for removing decimal part
+// console.log(otp);
+
+function swapNumbers(a, b) {
+  a = a + b;
+  b = a - b;
+  a = a - b;
+  return [a, b];
+}
+
+swapNumbers(5, 10);
