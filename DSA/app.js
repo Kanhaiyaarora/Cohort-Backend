149cd0117a81f7e6dbd6e84bfaf5d9c873825a32
Kanhaiya Arora <kanhaiyaarora75@gmail.com>
2026-06-18 21:53:00 +0530

    DSA

--- DSA/app.js
+++ DSA/app.js
@@ -1,7 +1,9 @@
 // 4 digit OTP generator
 
-// let otp = Math.trunc(Math.random() * 9000 + 1000); // 1000-9999 // trunc is used for removing decimal part
+// let otp = Math.trunc(Math.random() * 9000 + 1000); // 1000-9999  trunc is used for removing decimal part
 // console.log(otp);
+
+// ------------------------> <-----------------------
 
 // function swapNumbers(a, b) {
 //   a = a + b;
@@ -12,22 +14,58 @@
 
 // swapNumbers(5, 10);
 
+// ------------------------> <-----------------------
+
 //  if amount 0-5000 => 0% discount
 //  if amount 5001-7000 => 5% discount
 //  if amount 7001-9000 => 10% discount
 //  if amount > 9000 => 20% discount
-function amountPayable(amount){
-  if(amount > 0 && amount <= 5000){
-    return amount * 1// 0% discount
-  } else if(amount > 5000 && amount <= 7000){
-    return amount * 0.95// 5% discount    
-  }else if(amount > 7000 && amount <= 9000){
-    return amount * 0.90// 10% discount
-  }else if(amount > 9000){
-    return amount * 0.80 // 20% discount
-  }
+
+
+// function amountPayable(amount){
+//   if(amount > 0 && amount <= 5000){
+//     return amount * 1// 0% discount
+//   } else if(amount > 5000 && amount <= 7000){
+//     return amount * 0.95// 5% discount    
+//   }else if(amount > 7000 && amount <= 9000){
+//     return amount * 0.90// 10% discount
+//   }else if(amount > 9000){
+//     return amount * 0.80 // 20% discount
+//   }
+// }
+
+// console.log(amountPayable(10000));
+
+// ------------------------> <-----------------------
+
+// bottomUp approach for calculating bill amount for electricity consumption
+
+// let amount = 0;
+
+// function bijlikabill(units){
+//   if(units > 400){
+//     amount = (units - 400) * 13
+//     units = 400
+//   }
+//   if(units > 200 && units <= 400){
+//     amount = amount + (units - 200) * 8
+//     units = 200
+//   }
+//   if(units > 100 && units <= 200){
+//     amount = amount + (units - 100) * 6
+//     units = 100
+//   }
+  
+//   amount = amount + units * 4.2
+//   return amount;
+// }
+
+// console.log(bijlikabill(900));
+
+
+
+let i = 10;
+while (i % 3 !== 0) {
+    console.log(i);
+    i--;
 }
-
-
-
-console.log(amountPayable(10000));
